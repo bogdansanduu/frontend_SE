6aad097714947d726bbbc3f136ec1101cdbdd1b4
Sandu Mihai <mundarini0802@gmail.com>
2023-01-06 19:34:50 +0200

    added video games page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
 } from "react-router-dom";
 import HelloWorldPage from "./pages/HelloWorldPage";
 import SignInSide from "./pages/SignIn";
+import VideoGamesPage from "./pages/VideoGamesPage";
 
 const NotImplemented = () => {
     return <div>
@@ -23,7 +24,7 @@ function App() {
                 <Sidebar/>
                 <Switch>
                     <Route path="/hello_world" exact component={HelloWorldPage}/>
-                    <Route path="/video_games" exact component={NotImplemented}/>
+                    <Route path="/video_games" exact component={VideoGamesPage}/>
                     <Route path="/sign_in" exact component={SignInSide}/>
                     <Route path="/sign_out" exact component={NotImplemented}/>
                 </Switch>
